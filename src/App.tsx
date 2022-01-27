eec3b7f4c764a755f06d7af1213586a5838b3118
kj455 <kaji.ibuki45@gmail.com>
2022-01-27 20:00:13 +0900

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
+import { Footer } from './components/Footer';
 import { LifeGameField } from './components/LifeGameField';
 
 function App() {
   return (
     <div className="font-orbitron">
       <LifeGameField />
-      <div className="fixed right-8 bottom-8 text-slate-200 text-right flex flex-col gap-4">
-        <a href="https://github.com/kj455">Github</a>
-        <a href="https://twitter.com/kj4555">Twitter</a>
-        <h1 className="text-slate-200 text-3xl">Ibuki Kaji</h1>
+      <div className="fixed right-8 bottom-8">
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type Props = {};
+
+export const Footer: React.VFC = () => {
+  return (
+    <div className="flex flex-col gap-4 text-right text-slate-200">
+      <a className="flex-grow-0" href="https://zenn.dev/kj455">
+        Zenn
+      </a>
+      <a className="flex-grow-0" href="https://github.com/kj455">
+        Github
+      </a>
+      <a className="flex-grow-0" href="https://twitter.com/kj4555">
+        Twitter
+      </a>
+      <h1 className="text-slate-200 text-3xl">Ibuki Kaji</h1>
+    </div>
+  );
+};
