dc3522578f250a98c9baa0c21a0dfd2773d9385b
kj455 <kaji.ibuki45@gmail.com>
2022-01-27 20:57:09 +0900

    fix: light-mode style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 
 export const Footer: React.VFC = () => {
   return (
-    <div className="flex flex-col gap-4 text-right text-slate-200">
-      <a className="flex-grow-0" href="https://zenn.dev/kj455">
+    <div className="flex flex-col gap-4 text-right text-gray-700 dark:text-slate-200">
+      <a className="font-semibold flex-grow-0" href="https://zenn.dev/kj455">
         Zenn
       </a>
-      <a className="flex-grow-0" href="https://github.com/kj455">
+      <a className="font-semibold flex-grow-0" href="https://github.com/kj455">
         Github
       </a>
-      <a className="flex-grow-0" href="https://twitter.com/kj4555">
+      <a
+        className="font-semibold flex-grow-0"
+        href="https://twitter.com/kj4555"
+      >
         Twitter
       </a>
-      <h1 className="text-slate-100 text-3xl">Ibuki Kaji</h1>
+      <h1 className="text-gray-700 dark:text-slate-100 text-3xl font-bold">
+        Ibuki Kaji
+      </h1>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-type Props = {};
-
 export const Header: React.VFC = () => {
   return (
-    <div className="flex flex-col gap-4 text-left text-slate-100">
-      <p className="text-3xl">Web developer.</p>
-      <p className="text-slate-200">like: TypeScript</p>
-      <p className="text-slate-200">@ Cyberagent</p>
-      <p className="text-slate-200">@ Trusthub</p>
+    <div className="flex flex-col gap-4 text-left text-gray-700 dark:text-slate-100">
+      <p className="text-3xl font-bold">Web developer.</p>
+      <p className="dark:text-slate-200 font-semibold">Like: TypeScript</p>
+      <p className="dark:text-slate-200 font-semibold">@ Cyberagent</p>
+      <p className="dark:text-slate-200 font-semibold">@ Trusthub</p>
     </div>
   );
 };
